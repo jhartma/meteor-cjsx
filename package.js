b50a41bc49337df22c5d471c9b152d5a612b7a71
Chris Hitchcott <hitchcott@gmail.com>
2015-03-08 01:09:48 +0800

    update `coffee-react-transform` to 2.4.1

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
+var coffeeReactVersion = "2.4.1";
+
 Package.describe({
   summary: "Write React Components for Meteor in Coffeescript",
-  version: "0.0.1",
+  version: coffeeReactVersion,
   name: "jhartma:cjsx",
   git: "https://github.com/jhartma/meteor-csjx.git"
 });
@@ -13,5 +15,5 @@ Package.registerBuildPlugin({
   sources: [
     'compile-cjsx.js'
   ],
-  npmDependencies: {"coffee-react-transform": "2.0.0", "coffee-script": "1.7.1", "source-map": "0.1.32"}
+  npmDependencies: {"coffee-react-transform": coffeeReactVersion, "coffee-script": "1.7.1", "source-map": "0.1.32"}
 });
